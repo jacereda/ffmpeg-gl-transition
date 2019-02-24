3fd78e7cad964bde27b74d092af753d2651a4b7b
Jorge Acereda <jacereda@gmail.com>
2019-02-24 12:44:49 +0100

    Simplify, parameterize ffmpeg

--- default.nix
+++ default.nix
@@ -1,15 +1,12 @@
 { nixpkgs ?  import <nixpkgs> {}
 , useEGL ? true
+, ffmpeg ? nixpkgs.ffmpeg_4
 }:
 with nixpkgs;
-let
-  f = ffmpeg-full.override {
-    patches = [ ./ffmpeg.diff ];
-  };
-  egldef = if useEGL then "define" else "undef";
-
-in f.overrideAttrs (o: rec {
-  prePatch = o.prePatch + ''
+let egldef = if useEGL then "define" else "undef";
+in ffmpeg.overrideAttrs (o: rec {
+  patches = o.patches or [] ++ [ ./ffmpeg.diff ];
+  prePatch = o.prePatch or "" + ''
     cp ${./vf_gltransition.c} ./libavfilter/vf_gltransition.c
     substituteInPlace ./libavfilter/vf_gltransition.c \
       --replace "define GL_TRANSITION_USING_EGL" "${egldef} GL_TRANSITION_USING_EGL"
